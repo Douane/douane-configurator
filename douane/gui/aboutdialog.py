b012af5d080afd0bbc2ef604a90e3ca0ed2dd472
Guillaume Hain <zedtux@zedroot.org>
2014-05-01 12:38:53 +0200

    Update license

--- douane/gui/aboutdialog.py
+++ douane/gui/aboutdialog.py
@@ -11,7 +11,7 @@ class DouaneAboutDialog(Gtk.AboutDialog):
     self.set_version("UNKNOWN")
     self.set_copyright("Guillaume Hain")
     self.set_comments("Configurator for Douane firewall at application layer.")
-    self.set_license("LGPL")
+    self.set_license("Distributed under the GPL v2 license")
     self.set_authors(["Guillaume Hain"])
     self.set_website("http://douaneapp.com/")
     self.set_website_label("Douane website")
